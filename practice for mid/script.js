ec6234119cfd742b8a4ab7fddcab0f8fddd2f34b
Md Rufsan Jani Shanto <105555246+MdRufsan@users.noreply.github.com>
2024-09-28 00:27:34 +0600

    Add files via upload

---
+++ practice for mid/script.js
@@ -0,0 +1,34 @@
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const textarea = document.getElementById("textarea").value;
+    const file = document.getElementById('fileInput').value;
+        
+    // Check for required fields
+    if (!name || !email || !password || !confirmPassword || !gender ||!file) {
+        alert("Please fill in all required fields.");
+        return false;
+    }
+
+    // Check if passwords match
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return false;
+    }
+
+    // Validate email format (you can use a more robust email validation regex)
+    if (!/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,4})+$/.test(email)) {
+        alert("Invalid email address!");
+        return false;
+    }
+    else{
+        alert("Successful!!!!!");
+    }
+
+ 
+
+    return true;
+}
